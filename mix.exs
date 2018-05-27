55dabd7ef16e6a1bb65ed278f400f853f3b42035
weihu_chen <cctiger36@gmail.com>
2018-05-27 12:46:51 +0900

    support elixir 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixPubsubRedisZ.MixProject do
     [
       app: :phoenix_pubsub_redis_z,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
