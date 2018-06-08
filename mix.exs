7402eade640cb5e8da038848374f740b1eaabfdf
weihu_chen <cctiger36@gmail.com>
2018-06-08 15:38:22 +0900

    bump version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixPubsubRedisZ.MixProject do
   def project do
     [
       app: :phoenix_pubsub_redis_z,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
